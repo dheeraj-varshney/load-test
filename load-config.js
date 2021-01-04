1f85917bd50c1ecfe4e7e7c5da2784d667767171
kaushik <kaushik@dream11.com>
2021-01-04 13:48:07 +0530

    fix the load config

--- load-config.js
+++ load-config.js
@@ -1,6 +1,16 @@
 const loadConfig = {
-    ContestHomeQuery: 5,
-    meQuery: 2
+    ContestsPostLockQuery:1,
+    GetTeamPreview: 3,
+    MyContestsCarouselQuery: 3,
+    ContestHomeQuery: 3,
+    ContestHomeTabsQuery: 8,
+    TotalChatUnreadCountQuery: 8,
+    GetLiveStreamBanner: 7,
+    MyPromotionsQuery: 6,
+    RoundMessageQuery: 2,
+    CricketMiniScoreCardQuery: 2,
+    ProfilePicQuery: 2,
+    TeamCountQuery: 3
 }
 
 module.exports = loadConfig;
