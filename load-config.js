037a46c621c974c2664ef423994b4b528189df86
Shrey Mehta <shrey@dream11.com>
2021-05-12 16:43:44 +0530

    only contest home

--- load-config.js
+++ load-config.js
@@ -1,16 +1,5 @@
 const loadConfig = {
-    ContestsPostLockQuery:1,
-    GetTeamPreview: 3,
-    MyContestsCarouselQuery: 3,
-    ContestHomeQuery: 3,
-    ContestHomeTabsQuery: 8,
-    TotalChatUnreadCountQuery: 8,
-    GetLiveStreamBanner: 7,
-    MyPromotionsQuery: 6,
-    RoundMessageQuery: 2,
-    CricketMiniScoreCardQuery: 2,
-    ProfilePicQuery: 2,
-    TeamCountQuery: 3
-}
+  ContestHomeQuery: 10
+};
 
 module.exports = loadConfig;
